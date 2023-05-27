9dc9cdd4f63c47f20f9fd87739897f2d4384a1c1
CTY <cty@CTYdeMacBook-Pro.local>
2023-05-28 03:46:41 +0800

    init with zsh config

---
+++ zsh/zsh-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+ln -sf .zshrc ~/.zshrc
+ln -sf .p10k.zsh ~/.p10k.zsh
+source ~/.zshrc
